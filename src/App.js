7031924c8b324e10216c18a2065f0f922acd9881
Pratikesh-kumar <pratikeshkumar96@gmail.com>
2023-05-10 18:21:56 +0530

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,42 +1,53 @@
-
-import { Fragment } from 'react';
-import './App.css';
-import AddContact from './components/AddContact';
-import ContactList from './components/ContactList';
+import { Fragment, useState } from "react";
+import "./App.css";
+import AddContact from "./components/AddContact";
+import ContactList from "./components/ContactList";
+const DATA_URL =
+  "https://raw.githubusercontent.com/Pratikesh-kumar/http-request/master/src/data.json";
 function App() {
-  const contactList = [
-    {
-      id: 1,
-      contactName: 'Rohit Sharma',
-      contactNum: '9833445566',
+  const [contacts, setContacts] = useState([]);
+  const [isLoading, setIsloading] = useState(false);
+  const fetchContactHandler = () => {
+    setIsloading(true)
+    fetch(DATA_URL)
+      .then((rawResponse) => {
+        console.log(rawResponse);
+        return rawResponse.json();
+      })
+      .then((response) => {
+        console.log(response);
+        const data = response.data.map((contactData) => {
+          return {
+            id: contactData.id,
+            contactName: contactData.name,
+            contactNum: contactData.number,
+          };
+        });
+        setContacts(data);
+        setIsloading(false);
+      });
+  };
+  const addContactHandler = (contact) => {
+    console.log(contact);
+  };
+  let content = <p>no contacts available!!</p>;
 
-    },
-    {
-      id: 2,
-      contactName: 'Mohit Sharma',
-      contactNum: '8833445566',
-      
-    },
-    {
-      id: 3,
-      contactName: 'Raj Kumar',
-      contactNum: '7833444566',
-      
-    },
-  ];
+  if (contacts.length > 0) {
+    content = <ContactList contacts={contacts} />;
+  }
+  if(isLoading){
+    content=<p>loading please wait</p>
+  }
   return (
     <Fragment>
       <section>
         <button>add contact </button>
-        <button>fetch contact</button>
+        <button onClick={fetchContactHandler}>fetch contact</button>
       </section>
       <section>
-       <AddContact/>
+        <AddContact onAddContact={addContactHandler} />
       </section>
-      <section>
-       <ContactList contacts ={contactList}/>
-      </section>
-      
+      <section>{content}</section>
     </Fragment>
   );
 }
